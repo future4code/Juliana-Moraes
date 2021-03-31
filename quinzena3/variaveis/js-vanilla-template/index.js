09b89960b4c62d5feb56b5ff40214b172e347032
Juliana Moraes <juliana.hmoraes@gmail.com>
2021-03-30 22:58:00 -0300

    exercicio javascript aula10 variaveis

---
+++ quinzena3/variaveis/js-vanilla-template/index.js
@@ -0,0 +1,67 @@
+// 1.  10
+//     10, 5
+
+// 2. 10, 10, 10
+
+// var nome;
+// var idade;
+
+// ---------  exercicio 1  --------------
+
+// console.log(typeof idade)
+// console.log(typeof nome)
+
+// nome = prompt('qual o seu nome?')
+// idade = prompt('qual a sua idade?')
+
+// console.log('Olá ' + nome + 'você tem ' + idade + 'anos')
+
+
+// ---------  exercicio 2  --------------
+
+// var rua;
+// var numero;
+
+// infoUsuario = new Array(5);
+
+// infoUsuario[0] = prompt('Informe a sua rua?')
+// infoUsuario[1] = prompt('Informe o numero da sua casa?');
+// infoUsuario[2] = prompt('Informe o seu bairro');
+// infoUsuario[3] = prompt('Informe a sua cidade');
+// infoUsuario[4] = prompt('Informe o seu estado');
+
+// console.log('1. Sua rua é: ' + infoUsuario[0])
+// console.log('2. O número da sua casa é : ' + infoUsuario[1])
+// console.log('3. O seu bairro é o: ' + infoUsuario[2])
+// console.log('4. A sua cidade é: ' + infoUsuario[3])
+// console.log('5. O seu estado é o: ' + infoUsuario[4])
+
+// ---------  exercicio 3  --------------
+
+// var comidas = ['hamburgue', 'churrasco', 'pizza', 'sopa', 'peixe']
+
+// console.log(comidas[0], comidas[1], comidas[2], comidas[3], comidas[4])
+
+// console.log(comidas[0])
+// console.log(comidas[1])
+// console.log(comidas[2])
+// console.log(comidas[3])
+// console.log(comidas[4])
+
+// comidas[1] = prompt('Qual a sua comida preferida?')
+
+// console.log(comidas[0], comidas[1], comidas[2], comidas[3], comidas[4])
+
+// ---------  exercicio 4 --------------
+
+infoUsuario = new Array(3);
+
+infoUsuario[0] = prompt('O céu é rosa?')
+infoUsuario[1] = prompt('Você gosta de queijo?');
+infoUsuario[2] = prompt('você ingere bebida alcoolica?');
+
+console.log('O céu é rosa?' + infoUsuario[0])
+console.log('Você gosta de queijo?' + infoUsuario[1])
+console.log('você ingere bebida alcoolica?' + infoUsuario[2])
+
+
