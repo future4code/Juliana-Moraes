ecbe649b78732366011a69923476d120e179e935
Juliana Moraes <juliana.hmoraes@gmail.com>
2021-04-11 20:36:25 -0300

    projeto quinzena3 blackjack

--- quinzena3/blackjack/nossoJogo.js
+++ quinzena3/blackjack/nossoJogo.js
@@ -53,10 +53,126 @@
    }
 
     
+   // // -------------tentando resolver desafio 9---------------------------------
+
+   // console.log('Bem Vindo ao jogo de Black!');
+
+   // if(confirm("Quer iniciar uma nova rodada?")) {
+      
+   //    let usuarioCarta1 = comprarCarta();
+   //    let usuarioCarta2 = comprarCarta();
+   //    let cartaExtra 
+
+   //    while (usuarioCarta1 == "A" && usuarioCarta2 == "A"){
+   //       usuarioCarta1 = comprarCarta();
+   //       usuarioCarta2 = comprarCarta();
+   //       console.log('sorteio realizado');
+   //       break;
+   //    }
+
+   //       let pontosUsuario = usuarioCarta1.valor + usuarioCarta2.valor;
+
+   //       const computadorCarta1 = comprarCarta();
+   //       const computadorCarta2 = comprarCarta();
+
+   //       const pontosComputador = computadorCarta1.valor + computadorCarta2.valor;
+
+   //       while(pontosUsuario < 21){
+            
+   //       }
+
+   //       if(confirm('Suas cartas são: ' + usuarioCarta1.texto + 'e' + usuarioCarta2.texto + '. A carta revelada do computador é: ' + computadorCarta1.texto + '\n' + 'Deseja comprar mais uma carta?')){
+   //          cartaExtra = comprarCarta();
+   //          pontosUsuario = usuarioCarta1.valor + usuarioCarta2.valor + cartaExtra.valor;
+
+   //       }else{
+   //          console.log('entrou no else')
+   //       }
+         
+   //       console.log('Computador - cartas: ' + computadorCarta1.texto, computadorCarta2.texto + ' Pontuação ' + pontosComputador);
+
+   //       // if(pontosUsuario == pontosComputador){
+   //       //       console.log('Empate!');
+   //       // } else { 
+            
+   //       //    if(pontosUsuario > pontosComputador){
+   //       //       console.log('O usuário ganhou!'); 
+   //       //    } else {
+   //       //       console.log('O computador ganhou!')
+   //       //    }
+
+   //       // }
+
+   //    } else {
+   //    console.log('O jogo acabou!');
+   // }
 
 
+  // -------------DESAFIO ---------------------------------
 
+   
+   // console.log('Bem Vindo ao jogo de Black!');
 
+   //    if(confirm("Quer iniciar uma nova rodada?")) {
+         
+   //       let usuarioCarta1 = comprarCarta();
+   //       let usuarioCarta2 = comprarCarta();
+   //       cartaExtra = new Array(52)
+
+   //       while (usuarioCarta1 == "A" && usuarioCarta2 == "A"){
+   //          usuarioCarta1 = comprarCarta();
+   //          usuarioCarta2 = comprarCarta();
+   //          console.log('sorteio realizado');
+   //          break;
+   //       }
+
+   //          let pontosUsuario = usuarioCarta1.valor + usuarioCarta2.valor;
+
+   //          const computadorCarta1 = comprarCarta();
+   //          const computadorCarta2 = comprarCarta();
+
+   //          const pontosComputador = computadorCarta1.valor + computadorCarta2.valor;
+              
+   //             if(confirm('Suas cartas são: ' + usuarioCarta1.texto + 'e' + usuarioCarta2.texto + '. A carta revelada do computador é: ' + computadorCarta1.texto + '\n' + 'Deseja comprar mais uma carta?')){
+   //                cartaExtra.push(comprarCarta())
+   //                pontosUsuario = usuarioCarta1.valor + usuarioCarta2.valor + cartaExtra.valor;
+
+   //                while(pontosUsuario < 21){
+   //                   if(confirm('Deseja comprar mais uma carta?')){
+   //                      cartaExtra.push(comprarCarta()) 
+   //                   }
+   //                }
+   
+   //             }else{
+   //                console.log('entrou no else')
+   //             }
+            
+
+   //          if(confirm('Suas cartas são: ' + usuarioCarta1.texto + 'e' + usuarioCarta2.texto + '. A carta revelada do computador é: ' + computadorCarta1.texto + '\n' + 'Deseja comprar mais uma carta?')){
+   //             cartaExtra = comprarCarta();
+   //             pontosUsuario = usuarioCarta1.valor + usuarioCarta2.valor + cartaExtra.valor;
+
+   //          }else{
+   //             console.log('entrou no else')
+   //          }
+            
+   //          console.log('Computador - cartas: ' + computadorCarta1.texto, computadorCarta2.texto + ' Pontuação ' + pontosComputador);
+
+   //          // if(pontosUsuario == pontosComputador){
+   //          //       console.log('Empate!');
+   //          // } else { 
+               
+   //          //    if(pontosUsuario > pontosComputador){
+   //          //       console.log('O usuário ganhou!'); 
+   //          //    } else {
+   //          //       console.log('O computador ganhou!')
+   //          //    }
+
+   //          // }
+
+   //       } else {
+   //       console.log('O jogo acabou!');
+   //    }
 
    
 
